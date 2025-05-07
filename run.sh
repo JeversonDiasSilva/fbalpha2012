61479d6f27c6fb6a5290567d5cfd9c2d5961fdde
Jeverson Dias da Silva <jeversondiasdasilva@gmail.com>
2025-05-07 09:29:33 -0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,44 @@
 #!/bin/bash
+# Curitiba 06 de Maio de 2025
+# Editor: Jeverson Dias da Silva 
 
-echo "Hello world"
-sleep 5
-pkill xterm
+ROXO="\033[1;35m"
+VERDE="\033[1;92m"
+RESET="\033[0m"
+
+echo -e "${ROXO}INSTALAÇÃO DO SISTEMA FINAL BURN ALPHA 2012 PARA BATOCERA V40 E V41...${RESET}"
+echo -e "${ROXO}INICIANDO A INSTALAÇÃO...${RESET}"
+sleep 3
+
+cd /userdata/roms > /dev/null 2>&1
+mkdir -p fbalpha > /dev/null 2>&1
+
+echo -e "${ROXO}BAIXANDO PACOTES...${RESET}"
+wget https://github.com/JeversonDiasSilva/fbalpha2012/releases/download/V1.1/FBA1 > /dev/null 2>&1
+wget https://github.com/JeversonDiasSilva/fbalpha2012/releases/download/V1.1/FBA2 > /dev/null 2>&1
+sleep 3
+
+echo -e "${ROXO}INSTALANDO OS PACOTES...${RESET}"
+unsquashfs -d fbalpha FBA1 > /dev/null 2>&1
+unsquashfs -d fbalpha FBA2 > /dev/null 2>&1
+sleep 3
+
+echo -e "${ROXO}FINALIZANDO INSTALAÇÃO...${RESET}"
+mv "/userdata/roms/fbalpha/.dep/es_systems_fbalpha.cfg" /userdata/system/configs/emulationstation > /dev/null 2>&1
+chattr +i "/userdata/roms/fbalpha/.dep/es_systems_fbalpha.cfg"
+mv /userdata/roms/fbalpha/.dep/fbalpha2012_libretro.so /userdata/system/configs/retroarch/cores > /dev/null 2>&1
+ln -sf /userdata/system/configs/retroarch/cores/fbalpha2012_libretro.so /usr/lib/libretro/fbalpha2012_libretro.so > /dev/null 2>&1
+sleep 3
+
+echo -e "${ROXO}LIMPANDO ARQUIVOS TEMPORÁRIOS...${RESET}"
+rm FBA1 FBA2 > /dev/null 2>&1
+sleep 3
+
+echo -e "${ROXO}SALVANDO OVERLAY...${RESET}"
+batocera-save-overlay > /dev/null 2>&1
+sleep 3
+
+echo -e "${ROXO}INSTALAÇÃO CONCLUÍDA COM SUCESSO!${RESET}"
+sleep 2
+
+echo -e "${VERDE}@JCGAMESCLASSICOS${RESET}"
