cca49689030c5d6002d5eea5edf7cfd6411bff2a
Jeverson Dias da Silva <jeversondiasdasilva@gmail.com>
2026-02-02 11:51:02 -0300

    Change download path for es_systems.cfg
    Update the path for downloading es_systems.cfg.

--- fbneo.sh
+++ fbneo.sh
@@ -38,7 +38,7 @@ if [ -f /userdata/system/.dev/dependencias.zip ]; then
     sed -i '/<core default="true">fbneo<\/core>/a\                    <core>fbneo_old</core>' /userdata/system/.dev/dependencias.zip
 fi
 if [ -f /usr/share/emulationstation/es_systems.cfg ]; then
-     wget -O /usr/share/emulationstation/es_systems.cfg \
+     wget -O /userdata/system/configs/emulationstation/es_systems.cfg \
 https://github.com/JeversonDiasSilva/fbalpha2012/releases/download/V1.1/es_systems.cfg > /dev/null 2>&1
 fi
 
